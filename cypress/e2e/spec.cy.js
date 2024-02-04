d42b38d8307c689977c1c45563640952d528957f
clemgrom <clement.gromangin5@etu.univ-lorraine.fr>
2024-02-04 20:27:16 +0100

    création de test avec cypress

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,59 @@
+
+describe('My First Test', () => {
+  it('visits the app root url', () => {
+    cy.visit('/')
+    cy.contains('h1', 'You did it!')
+  })
+})
+
+describe('Page Test', () => {
+  it('Visite la page et vérifie la présence de l\'élément #app', () => {
+    cy.visit('http://localhost:3000')
+    cy.get('#app').should('exist') 
+  })
+})
+
+describe('Page Test', () => {
+  it('Visite la page et vérifie la présence de l\'élément h3', () => {
+    cy.visit('http://localhost:3000')
+    cy.get(':nth-child(1) > .details > h3')
+      .should('exist')
+      .and('have.text', 'Create Vue project with Vite')
+  })
+})
+
+describe('Page Test', () => {
+  it('Visite la page et vérifie la présence de l\'élément Create Workflow', () => {
+    cy.visit('http://localhost:3000')
+    cy.get(':nth-child(4) > .details')
+      .should('exist')
+  })
+})
+
+describe('Page Test', () => {
+  it('Visite la page et vérifie la présence de l\'élément #app', () => {
+    cy.visit('http://localhost:3000/about')
+    cy.get('.about > h1')
+      .should('exist')
+      .and('have.text', 'This is an about page')
+  })
+})
+
+describe('Page Test', () => {
+  it('Visite la page et vérifie la présence de l\'élément logo', () => {
+    cy.visit('http://localhost:3000/about')
+    cy.get('.logo')
+      .should('exist')
+     
+  })
+})
+
+
+describe('Page Test', () => {
+  it('Visite la page et vérifie la présence de l\'élément .green', () => {
+    cy.visit('http://localhost:3000/about')
+    cy.get('.green')
+     .should('exist')
+     .and('have.text', 'You did it!')
+  })
+})
