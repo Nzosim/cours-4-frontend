7236e55496ab0c35f4addbc8210bca85ab5fd534
clemgrom <clement.gromangin5@etu.univ-lorraine.fr>
2024-02-04 19:47:03 +0100

    test e2e

--- cypress/e2e/example.cy.js
+++ cypress/e2e/example.cy.js
@@ -6,3 +6,18 @@ describe('My First Test', () => {
     cy.contains('h1', 'You did it!')
   })
 })
+
+describe('Mon premier test', () => {
+  it('Visite la page d\'accueil', () => {
+    cy.visit('http://localhost:3000') // changez ceci à l'URL de votre application
+  })
+})
+
+describe('Page Test', () => {
+  it('Visite la page et vérifie la présence de l\'élément #app', () => {
+    cy.visit('http://localhost:3000')
+    cy.get('#app').should('exist') 
+  })
+})
+
+
